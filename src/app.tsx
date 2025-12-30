dcd358ad76ce432dd1c0adc9195ceb0ec1ba3677
yamanidev <elamine.yamani@gmail.com>
2025-12-30 20:40:34 +0100

    chore: add Prettier configuration file + format project

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import viteLogo from "/vite.svg";
+import "./app.css";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,26 +18,21 @@ export function App() {
       </div>
       <h1>Vite + Preact</h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/app.tsx</code> and save to test HMR
         </p>
       </div>
       <p>
-        Check out{' '}
+        Check out{" "}
         <a
           href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
+          target="_blank">
           create-preact
         </a>
         , the official Preact + Vite starter
       </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <p class="read-the-docs">Click on the Vite and Preact logos to learn more</p>
     </>
-  )
+  );
 }
